fc6685ee41ab714523326ddd1368d04e502de834
kholishotula <kholishotula.ka@gmail.com>
2021-01-14 11:08:19 +0700

    final

--- env.php
+++ env.php
@@ -1,11 +1,6 @@
 <?php
 defined('env') OR exit('Akses langsung ke Skrip ini diblokir');
 
-$setDb['db_host']='127.0.0.1';
-$setDb['db_name']='COVID19';
-$setDb['db_user']='root';
-$setDb['db_password']='';
-
 // folder templates
 $template='templates/AdminLTE-2.4.17/';
 
